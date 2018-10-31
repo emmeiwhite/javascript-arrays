80ab7044297828529579090044d2d475c83b05c1
emmeiwhite <emmeiwhite@gmail.com>
2018-10-31 14:13:07 +0530

    Immutabilty in arrays, pushing,poping,removing etc

---
+++ add_remove_array_elements.js
@@ -0,0 +1,9 @@
+const colors = ['red','orange','blue'];
+
+console.log(colors);
+
+colors.push('green');
+
+console.log(colors); //push / pop /shift /unshift works mutably. concat() works immutabily
+
+
